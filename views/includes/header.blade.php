30ce1c9bc2a22eee6480310c17c2cde013a1e96b
Armstrong Lohãns <lohansdemelo1108@hotmail.com>
2018-10-25 16:22:47 -0300

    Aleteração da página inicial

---
+++ views/includes/header.blade.php
@@ -0,0 +1,8 @@
+<header class="masthead bg-white text-secondary text-center" id="inicio">
+    <div class="container">
+        <img class="img-fluid mb-5 d-block mx-auto" src="img/logoRedondo.png" alt="" style="width:250; height:250px;">
+        <h1 class="text-uppercase mb-0">Gestão de Eventos</h1>
+        <hr class="linha-escura-completa">
+        <h2 class="font-weight-light mb-0">Fique por dentro dos próximos eventos!</h2>
+    </div>
+</header>
